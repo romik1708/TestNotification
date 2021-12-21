69503df1e070de62a2f67994ab80b257c3d96a47
NP-SOFTTECH-PC\NP SoftTech <gofkeapp@gmail.com>
2021-12-21 18:37:05 +0530

    Add Library Module

---
+++ TestNotification/src/main/java/com/example/testnotification/NotiDemo.java
@@ -0,0 +1,12 @@
+package com.example.testnotification;
+
+import android.app.Activity;
+import android.util.Log;
+
+public class NotiDemo {
+
+    NotiDemo()
+    {
+        Log.e("activity--)","Zuch");
+    }
+}
